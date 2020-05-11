c0901902b16a4da69283250028262782c50f6924
ewanb72 <ewanb72@gmail.com>
2020-05-11 18:26:54 +0100

    Get rid of unused files and move TestPosters out of the website and keep for later

--- moviemash-backend/api/getMovies.php
+++
@@ -1,6 +0,0 @@
-<?php
-//TODO: Dont know if we need these headers. Maybe issues with CORS
-//header('Access-Control-Allow-Origin: *');
-//header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE');
-
-echo "this is response";
